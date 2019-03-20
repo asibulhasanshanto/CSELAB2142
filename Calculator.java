ac7e315084a64a10fd3d64e42992c262381d8593
Asibul Hasan <asibulhasan.rucse.18@gmail.com>
2019-03-20 10:33:08 +0600

    Addition and subtraction calculator

---
+++ Calculator.java
@@ -0,0 +1,11 @@
+public class Calculator {
+    public static void main(String[] args){
+
+    }
+    float Addition(float x,float y){
+        return x+y;
+    }
+    float Subtraction(float x,float y){
+        return x-y;
+    }
+}
